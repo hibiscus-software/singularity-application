8b8f26db36ac127112be70a2ec1ed681140f851b
Cody Wellman <cody@codexmicro.systems>
2024-02-04 22:59:53 -0500

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C), 2023 Codex Microsystems. Some rights reserved. This work is
+ * licensed under the terms of the MIT license which can be found in the
+ * root directory of this project.
+ */
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C), 2023 Codex Microsystems. Some rights reserved. This work is
+ * licensed under the terms of the MIT license which can be found in the
+ * root directory of this project.
+ */
+
+export default function Page() {
+  return <h1>Hello, World!</h1>
+}
